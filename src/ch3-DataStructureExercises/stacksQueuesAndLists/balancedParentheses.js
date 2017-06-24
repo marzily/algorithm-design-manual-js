2d60c9f555dd612e432f8e28050d408765bb9a37
margie <ly.margarett@gmail.com>
2017-06-23 23:52:16 -0700

    index.html properly displays tests, update tests to use "assert" instead of "expect"

--- src/ch3-DataStructureExercises/stacksQueuesAndLists/balancedParentheses.js
+++ src/ch3-DataStructureExercises/stacksQueuesAndLists/balancedParentheses.js
@@ -1,6 +1,6 @@
 /* Give an algorithm that returns true if a string contains properly nested and balanced parentheses, and false otherwise. For full credit, identify the position of the first offending parenthesis if the string is not properly nested and balanced. */
 
-exports.properlyNested = function(string) {
+var properlyNested = function(string) {
   var indexedParens = [];
 
   for (var i = 0; i < string.length; i++) {
--- test/ch3-DataStructureExercises/stacksQueuesAndLists/balancedParenthesesTest.js
+++ test/ch3-DataStructureExercises/stacksQueuesAndLists/balancedParenthesesTest.js
@@ -1,32 +1,29 @@
-var expect = require("chai").expect;
-var parens = require("../../../src/ch3-DataStructureExercises/stacksQueuesAndLists/balancedParentheses");
-
-describe("Balanced Parentheses", function() {
+describe("1. Balanced Parentheses", function() {
   it("returns true when parentheses are properly nested", function() {
     var input = "((())())()";
-    var result = parens.properlyNested(input);
+    var result = properlyNested(input);
 
-    expect(result).to.be.true;
+    assert(result);
   });
 
   it ("returns first index when parentheses are not properly nested", function () {
     var input = ")()(";
-    var result = parens.properlyNested(input);
+    var result = properlyNested(input);
 
-    expect(result).to.equal(0);
+    assert.equal(result, 0);
   });
 
   it ("returns first index when parentheses are not closed", function () {
     var input = "())";
-    var result = parens.properlyNested(input);
+    var result = properlyNested(input);
 
-    expect(result).to.equal(2);
+    assert.equal(result, 2);
   });
 
   it ("returns first index when there is a hanging parenthesis", function() {
     var input = "(";
-    var result = parens.properlyNested(input);
+    var result = properlyNested(input);
 
-    expect(result).to.equal(0);
+    assert.equal(result, 0);
   });
 });
