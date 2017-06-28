5552c38eb2e63d4c6b1a4f18e4cab58b5d870dd9
margie <ly.margarett@gmail.com>
2017-06-28 00:11:17 -0700

    wrap hash table tests in describe block

--- test/ch3-DataStructures/treesAndOtherDictionaryStructures/hashTableTest.js
+++ test/ch3-DataStructures/treesAndOtherDictionaryStructures/hashTableTest.js
@@ -2,29 +2,31 @@ var assert = require('../../../_vendor/chai').assert;
 var hashTable = require('../../../src/ch3-DataStructures/treesAndOtherDictionaryStructures/hashTable');
 
 describe("4. Hash Table", function() {
-  var hash = new hashTable(3);
+  describe("basic attributes", function() {
+    var hash = new hashTable(3);
 
-  it("initializes with an input size", function() {
-    assert.equal(hash.inputSize, 3);
-  });
+    it("initializes with an input size", function() {
+      assert.equal(hash.inputSize, 3);
+    });
 
-  it("has a load factor", function() {
-    assert.equal(hash.loadFactor, 0.75);
-  });
+    it("has a load factor", function() {
+      assert.equal(hash.loadFactor, 0.75);
+    });
 
-  it("has bins", function() {
-    assert.isArray(hash.bins);
-  });
+    it("has bins", function() {
+      assert.isArray(hash.bins);
+    });
 
-  it("sizes bins based on its input size ", function() {
-    assert.equal(hash.bins.length, 8);
-  });
+    it("sizes bins based on its input size ", function() {
+      assert.equal(hash.bins.length, 8);
+    });
 
-  it("resizes bins based on its input size ", function() {
-    hash.resetSize(9);
+    it("resizes bins based on its input size ", function() {
+      hash.resetSize(9);
 
-    assert.equal(hash.inputSize, 9);
-    assert.equal(hash.bins.length, 24);
+      assert.equal(hash.inputSize, 9);
+      assert.equal(hash.bins.length, 24);
+    });
   });
 
   xdescribe("hash table operations", function() {
