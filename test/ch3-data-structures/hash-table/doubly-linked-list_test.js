31cd0961c82d9daab0f67b7c5c5ae5031544ac1d
margie <ly.margarett@gmail.com>
2017-06-30 11:47:51 -0700

    create files and tests for doubly linked list node

--- test/ch3-data-structures/hash-table/doubly-linked-list_test.js
+++ test/ch3-data-structures/hash-table/doubly-linked-list_test.js
@@ -28,12 +28,16 @@ describe("Doubly Linked List", function() {
       assert.equal(head.next.prev.data, 1);
     });
 
-    xit("tracks how many nodes there are in a list", function() {
+    it("tracks how many nodes there are in a list", function() {
       assert.equal(head.length(), 2);
 
       head.append(3);
 
       assert.equal(head.length(), 3);
+    });
+
+    it("has a pointer to its tail", function() {
+
     });
   });
 });
---
+++ test/ch3-data-structures/hash-table/node_test.js
@@ -0,0 +1,18 @@
+var assert = require('../../../_vendor/chai').assert;
+var Node = require('../../../src/ch3-data-structures/hash-table/node');
+
+describe("Doubly Linked List Node", function() {
+  var node = new Node(1);
+
+  it("has data", function() {
+    assert.equal(node.data, 1);
+  });
+
+  it("has a pointer to a previous node", function() {
+    assert.equal(node.prev, null);
+  });
+
+  it("has a pointer to a next node", function() {
+    assert.equal(node.next, null);
+  });
+});
